89d7d0f42cc643dd48bb9f922d4d5073e7bd2914
Dai <2524918410@qq.com>
2020-03-08 15:20:32 +0800

    数据库几种结构的实现

---
+++ MyDataBase/src/mystack/test.java
@@ -0,0 +1,19 @@
+package mystack;
+
+public class test {
+    public static void main(String[] args) {
+        myStack<Integer> ms = new myStack<>();
+        System.out.println(ms.empty());
+        ms.push(5);
+        ms.push(4);
+        ms.push(3);
+        ms.push(2);
+        ms.push(1);
+        System.out.println(ms.empty());
+        System.out.println(ms.size());
+        System.out.println(ms.peek());
+        ms.pop();
+        System.out.println(ms.size());
+        System.out.println(ms.peek());
+    }
+}

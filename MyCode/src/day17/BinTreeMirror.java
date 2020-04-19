2e4db3bbb38c3f868c2e66f1d7059eee487912cf
Dai <2524918410@qq.com>
2020-04-19 16:01:40 +0800

    1. 二叉树镜像

---
+++ MyCode/src/day17/BinTreeMirror.java
@@ -0,0 +1,27 @@
+package day17;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
+
+
+public class BinTreeMirror {
+    public void Mirror(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        Mirror(root.left);
+        Mirror(root.right);
+
+    }
+}
